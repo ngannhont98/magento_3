9f41b95340d765fe0af79ffb49586d19baf421cf
ngannhont98 <nganphan.300798@gmail.com>
2019-10-09 18:33:49 +0700

    updating menu

--- design/frontend/Smartosc/melissa/registration.php
+++ design/frontend/Smartosc/melissa/registration.php
@@ -6,4 +6,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Smartosc/melisa', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Smartosc/melissa', __DIR__);
